e2b6072943754fa1a273541fcc733ad9393441b6
Muhammad Zeeshan <zee.1mughal@gmail.com>
2022-10-18 14:30:14 +0500

    Initial Commit

---
+++ PopMeals/PopMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopMealsApp.swift
+//  PopMeals
+//
+//  Created by Muhammad Zeeshan on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PopMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
